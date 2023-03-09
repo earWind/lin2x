33616c655bfb46687142de58b0dbd3cf2aba7c64
linjuncai <linjuncai>
2023-03-09 16:26:38 +0800

    microapp

--- QianKun/mini-microapp/index.js
+++ QianKun/mini-microapp/index.js
@@ -8,11 +8,15 @@
  */
 
 let apps = [];
+let prevRoute = "";
+let nextRoute = "";
 
+// 注册路由
 function registerMicroApps(data) {
   apps = data;
 }
 
+// 入口
 function start() {
   // 1.监听路由变化
 
@@ -34,21 +38,47 @@ function start() {
   // });
 
   const rawPushState = window.history.pushState;
-  window.history.pushState = (...args) => {
+  window.history.pushState = async (...args) => {
+    // 导航前
+    prevRoute = window.location.hash;
+    await unmountPrevRoute();
+
     rawPushState.apply(window.history, args);
     // console.log("监听 pushState");
+
+    // 导航后
+    nextRoute = window.location.hash;
     routerChange();
   };
+
   const rawReplaceState = window.history.replaceState;
   window.history.replaceState = (...args) => {
     rawReplaceState.apply(window.history, args);
     // console.log("监听 replaceState");
   };
+
+  routerChange();
+  prevRoute = window.location.hash;
 }
 
+// 销毁上一应用
+async function unmountPrevRoute() {
+  for (let i = 0; i < apps.length; i++) {
+    const prevApp = apps[i];
+    if (prevRoute.endsWith(prevApp.activeRule)) {
+      if (prevApp && prevApp.unmount) {
+        // 卸载子应用
+        await prevApp.unmount();
+      }
+    }
+  }
+}
+
+// 渲染应用
 async function routerChange() {
   for (let i = 0; i < apps.length; i++) {
     const app = apps[i];
+
     // 2.匹配子应用
     if (window.location.hash.endsWith(app.activeRule)) {
       const { template, execScripts } = await importHtml(app.entry);
@@ -56,15 +86,26 @@ async function routerChange() {
       const container = document.querySelector(app.container);
       container.appendChild(template);
 
-      execScripts();
+      const appExports = await execScripts();
+      app.bootstrap = appExports.bootstrap;
+      app.mount = appExports.mount;
+      app.unmount = appExports.unmount;
+
+      // 渲染前
+      await microBootstrap(app);
+
+      // 渲染
+      await microMount(app);
     }
   }
 }
 
+// 请求html资源
 async function fetchResource(url) {
   return await fetch(url).then((res) => res.text());
 }
 
+// 加载应用（html）并加载html里面的script
 async function importHtml(url) {
   // 3.加载子应用
   const html = await fetchResource(url);
@@ -93,6 +134,7 @@ async function importHtml(url) {
   async function execScripts() {
     const scriptCodes = await getExternalScripts();
     scriptCodes.forEach(eval);
+    return window.exports;
   }
 
   return {
@@ -100,4 +142,24 @@ async function importHtml(url) {
     getExternalScripts,
     execScripts,
   };
+}
+
+async function microBootstrap(app) {
+  if (app.bootstrap) {
+    await app.bootstrap();
+  }
+}
+
+async function microMount(app) {
+  if (app.mount) {
+    await app.mount({
+      container: document.querySelector(app.container),
+    });
+  }
+}
+
+async function microUnmount(app) {
+  if (app.unmount) {
+    await app.unmount();
+  }
 }
