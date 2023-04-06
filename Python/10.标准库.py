afa10eb48ba15f140a806894ce3cd5b326046ba1
linjuncai <linjuncai>
2023-04-06 17:30:34 +0800

    1

--- Python/10.标准库.py
+++ Python/10.标准库.py
@@ -1,3 +1,7 @@
+import reprlib
+from timeit import Timer
+import zlib
+from datetime import date
 from urllib.request import urlopen
 import glob
 import os
@@ -13,6 +17,28 @@ print(glob.glob('*.py'))
 f = open('D:\_git\lin2x\Python\workfile.txt', 'w', encoding="utf-8")
 with urlopen('https://www.baidu.com/') as response:
     for line in response:
-        line = line.decode()
-        print(type(line))
+        line = line.decode()  # Convert bytes to a str
         f.write(line)
+
+# 日期和时间
+now = date.today()
+print(now)
+print(now.strftime("%m-%d-%y. %d %b %Y is a %A on the %d day of %B."))
+birthday = date(1964, 7, 31)
+print(birthday)
+
+# 数据压缩
+s = b'witch which has which witches wrist watch'
+len(s)
+
+t = zlib.compress(s)
+print(len(t))
+print(zlib.decompress(t))
+print(zlib.crc32(s))
+
+# 性能测量
+print(Timer('t=a; a=b; b=t', 'a=1; b=2').timeit())
+print(Timer('a,b = b,a', 'a=1; b=2').timeit())
+
+# 格式化输出
+reprlib.repr(set('supercalifragilisticexpialidocious'))
